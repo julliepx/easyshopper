fa7dcdbea9b9c1e2750f3c5603279449662c549f
Jullie <jullie_ferreira@sicredi.com.br>
2025-04-06 02:25:57 +0200

    config: initializing project

---
+++ src/main/java/com/extia/easyshopper/EasyshopperApplication.java
@@ -0,0 +1,13 @@
+package com.extia.easyshopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyshopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyshopperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/extia/easyshopper/EasyshopperApplicationTests.java
@@ -0,0 +1,6 @@
+package com.extia.easyshopper;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyshopperApplicationTests {}
